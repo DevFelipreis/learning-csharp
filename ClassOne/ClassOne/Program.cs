d65e70aeee8d766674871a0a0c3d6dfa7b6e306e
DevFelipreis <lfreis.contato@gmail.com>
2024-07-19 11:35:25 -0300

    other commit

---
+++ ClassOne/ClassOne/Program.cs
@@ -0,0 +1,19 @@
+﻿
+namespace ClassOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte x = 100;
+            Console.WriteLine(x);
+
+            char feminino = 'f';
+            Console.WriteLine(feminino);
+
+            string name = "Felipe";
+            Console.WriteLine(name);
+
+        }
+    }
+}
